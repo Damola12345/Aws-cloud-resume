cd6573126b59851ce0885a5aac9daae805d1f6ed
Damola12345 <ajiboladamola789@gmail.com>
2021-09-29 12:37:23 +0100

    updated js

--- visitorcount.js
+++ visitorcount.js
@@ -1,6 +1,6 @@
 /*----------- get vistors count -----------------*/
 function myFunction(){
-    fetch("https://api.rajanpanchal.net/Prod/getVisitorCounter")
+    fetch("https://8v5ru96fbe.execute-api.us-east-1.amazonaws.com/Prod/get-visitor")
     .then(response => response.text())
     .then((body) => {
       document.getElementById("visitor").innerHTML=body
