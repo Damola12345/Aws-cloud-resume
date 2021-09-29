3c3052eb3d3ae39e7cacb16b6b223b803eddedfa
Damola12345 <ajiboladamola789@gmail.com>
2021-09-29 13:08:57 +0100

    updated js&html

--- visitorcount.js
+++ visitorcount.js
@@ -3,7 +3,7 @@ function myFunction(){
     fetch("https://8v5ru96fbe.execute-api.us-east-1.amazonaws.com/Prod/get-visitor")
     .then(response => response.text())
     .then((body) => {
-      document.getElementById("visitor").innerHTML=body
+      document.getElementById("visits").innerHTML=body
     })
     .catch(function(error) {
       console.log(error); 
